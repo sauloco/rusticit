47cef0022a86e17207193fbf4ec00ddb7664b30a
Florencia Soncini <florenciasoncini@gmail.com>
2020-06-25 11:01:53 -0300

    handle mode

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-
-document.querySelectorAll(".menu, nav a, nav img").forEach(e=> e.addEventListener ("click", toggleNav));
+document.addEventListener('DOMContentLoaded', handleMode)
+document
+  .querySelectorAll(".menu, nav a, nav img")
+  .forEach((e) => e.addEventListener("click", toggleNav));
 document.querySelector(".toggle-mode").addEventListener("click", toggleDark);
 
 function toggleNav() {
-    document.querySelector("nav").classList.toggle("hide");
+  document.querySelector("nav").classList.toggle("hide");
 }
 
 function toggleDark() {
-    document.querySelector("body").classList.toggle("dark-mode");
-    document.querySelector("body").classList.toggle("light-mode");
+  document.querySelector("body").classList.toggle("dark-mode");
+  document.querySelector("body").classList.toggle("light-mode");
 }
 
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-darkModeMediaQuery.addListener((e) => {
-  const darkModeOn = e.matches;
-  console.log(`Dark mode is ${darkModeOn ? '🌒 on' : '☀️ off'}.`, darkModeOn);
-  toggleDark()
-});
-console.log(darkModeMediaQuery)
+function handleMode() {
+  if (window.matchMedia) {
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+    darkModeMediaQuery.addListener((e) => {
+      toggleDark();
+    });
+    if (darkModeMediaQuery.matches) {
+      toggleDark();
+    }
+  }
+}
