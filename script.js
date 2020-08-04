9f1d53403fbed4bee0605b3aa3235ced9f5e75e2
Florencia Soncini <florenciasoncini@gmail.com>
2020-08-04 10:18:32 -0300

    language - details

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ document
   .querySelectorAll(".menu, nav a, nav img")
   .forEach((e) => e.addEventListener("click", toggleNav));
 document.querySelector(".toggle-mode").addEventListener("click", toggleDark);
-//document.querySelector("toggle-language").addEventListener("click", toggleLanguage)
+document.querySelector(".toggle-language").addEventListener("click", toggleLanguage)
+
+let language = 'en';
+
 
 function toggleNav() {
   document.querySelector("nav").classList.toggle("hide");
@@ -43,8 +46,8 @@ function toggleLanguage() {
     },
     {
       selector: "#menu-language",
-      html_en: "english",
-      html_es: "español",
+      html_en: "español",
+      html_es: "english",
     },
     {
       selector: "#menu-dark",
@@ -59,8 +62,7 @@ function toggleLanguage() {
     {
       selector: "#hero-one",
       html_en: "We build robust yet modern digital experiences",
-      html_es:
-        "Construimos experiencias digitales robustas, seguras y modernas",
+      html_es: "Construimos experiencias digitales robustas, seguras y modernas",
     },
     {
       selector: "#hero-two",
@@ -73,24 +75,41 @@ function toggleLanguage() {
       html_es: "Consultoría y creación de soluciones.",
     },
     {
+      selector: "#hero-input",
+      html_en: "Just type you mail and/or phone number here and we will contact you.",
+      html_es: "Escribe aquí tu correo electrónico o número de teléfono y te contactaremos"
+    },
+    {
       selector: "#great-idea-t",
       html_en: "Share your <br><span>great idea</span><br> with us",
       html_es: "Hagamos realidad tu <span>gran idea</span>",
     },
     {
-      selector: "#grat-idea-p",
-      html_en: "",
-      html_es: "",
+      selector: "#great-idea-p",
+      html_en: `You know how much that idea worth, it has potential... if you only knew the recipe to turn it real in the ` +
+      `digital world. Well, a great idea, like yours, is a great start to make a bussiness, but sadly that's not enough. ` +
+      `We want to help you! We offer you all our knowledge in digital experiences, design and marketing, ` +
+      `that way you will see your idea becoming real, worth the shot? Contact us`,
+      html_es: `Tú sabes que vale mucho esa idea, que tiene potencial... si tan solo supieras dar todos los pasos necesarios para ` +
+      `hacerla digital. Una buena idea es un gran puntapié para iniciar un negocio, pero lamentablemente con eso solo no alcanza. ` +
+      `¡Queremos ayudarte! Para eso ponemos a tu disposición todos nuestros conocimientos en experiencias, diseño y ` +
+      `marketing digital, para que puedas ir haciendo realidad tus ideas. ¡Vale la pena intentarlo! Consúltanos.`,
     },
     {
       selector: "#think-t",
       html_en: "Let us <br><span>think</span> about it",
-      html_es: "",
+      html_es: "Danos unos días para <br><span>pensar</span> la solución",
     },
     {
       selector: "#think-p",
-      html_en: "",
-      html_es: "",
+      html_en: `As valuable as your ideas are for you, also they are for us. Then after you reach us we will set a call and ` + 
+      `after that we will invest our time to think what's the optimal solution for you to reach your goals. ` +
+      `Sometimes will be mandatory to develop a custom solution, but sometimes we will help you through the implementation ` + 
+      `and adaptation of an existing digital tool. In any case we will be there to make whatever it takes. Contact us`,
+      html_es: `Tus ideas son extremadamente valiosas, para ello contáctanos, organicemos una llamada para conocer los detalles, ` + 
+      `pero luego invertiremos de nuestro tiempo en pensar cuál es la solución óptima para lograr tu objetivo. ` +
+      `A veces será necesario desarrollar una solución, a veces será necesario adaptar una solución ya existente, ` +
+      `en cualquier caso estaremos ahí para hacer lo que haga falta. ¡Consultanos!`,
     },
     {
       selector: "#design-t",
@@ -99,8 +118,13 @@ function toggleLanguage() {
     },
     {
       selector: "#design-p",
-      html_en: "",
-      html_es: "",
+      html_en: `Do you have the idea and nothing more? Not an issue, we will start helping you to shape it and design it until it becomes reality. ` +
+      `Do you have the design too? Great! Following that guides we can focus to bring it to the next level, adapting it ` +
+      `for digital media.`,
+      html_es: `Tienes solo la idea, no es inconveniente, comenzaremos ayudándote a diseñar todos los aspectos para convertirla en realidad. ` +
+      `¿Tienes el diseño? ¡Excelente! Respetando esas guías podremos concentrarnos en llevarla al siguiente nivel, ` +
+      `adaptándola a todos los medios digitales. ` +
+      `En cualquier caso, ¡consúltanos!`,
     },
     {
       selector: "#mess-t",
@@ -109,8 +133,14 @@ function toggleLanguage() {
     },
     {
       selector: "#mess-p",
-      html_en: "",
-      html_es: "",
+      html_en: `This idea of yours will become in a web site, mobile app, social media profiles, advertisment and coordinate posts, `+ 
+      `servers, security... looks overwelming? Don't worry, we want you focus in your business, not in technical details. ` +
+      `We partner with the best companies, and use their best resources to bring you highly reliable services and ` +
+      `availability, then your idea can grow at your own pace.`,
+      html_es: `Esta idea se transformará en una página web, app móvil, perfiles en redes sociales, servidores, seguridad... puede ` + 
+      `parecer abrumador, pero nosotros queremos que te enfoques en tu negocio, no en los detalles técnicos. ` +
+      `Nos asociamos con empresas que brindan servicios de alta confiabilidad y disponibilidad, para que tu idea pueda ` + 
+      `crecer al ritmo que necesitas. Consulta sobre este tema`,
     },
     {
       selector: "#want-more-t",
@@ -118,14 +148,47 @@ function toggleLanguage() {
       html_es: "Quieres</span> saber un poco <span>más",
     },
     {
-      selector: "",
-      html_en: "",
-      html_es: "",
+      selector: "#more-input-name",
+      html_en: "Type your name or company name here",
+      html_es: "Escribe tu nombre o el nombre de tu empresa aquí"
+    },
+    {
+      selector: "#more-input-mail",
+      html_en: "Write your mail or phone here",
+      html_es: "Escribe tu correo electrónico o número de teléfono aquí"
+    },
+    {
+      selector: "#more-input-tell",
+      html_en: "Tell us a bit about your idea",
+      html_es: "Cuéntanos un poco sobre lo que necesitas"
+    },
+    {
+      selector: "#more-send",
+      html_en: "Ok, send",
+      html_es: "Enviar"
     },
   ];
 
+  if (language === 'en') {
+    language = 'es';
+  } else {
+    language = 'en';
+  }
+
   for (const sentence of sentences) {
-    document.querySelector(sentence.selector).innerHTML(sentence.html);
+    const element = document.querySelector(sentence.selector);
+    const value = sentence["html_"+language];
+
+    if (!element) {
+      console.error(sentence.selector, 'not found');
+      continue;
+    }
+
+    if (element.tagName === "INPUT") {
+      element.placeholder = value;
+    } else {
+      element.innerHTML = value;
+    }
   }
 }
 
