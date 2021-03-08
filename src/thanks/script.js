bd19b0ad2a6164baeb0ee49aca4007ea3947a95b
Martín <soncinimartin@gmail.com>
2021-03-08 14:23:27 -0300

    object services file set service name in html from url params

--- src/thanks/script.js
+++ src/thanks/script.js
@@ -1,11 +1,51 @@
 import { setPreferredLanguage, toggleLanguage } from "../i18n";
-import { toggleNav, toggleDark, handleMode } from "../script";
+// import { toggleNav, toggleDark, handleMode } from "../script";
+
 document.addEventListener("DOMContentLoaded", handleMode);
 document.addEventListener("DOMContentLoaded", setPreferredLanguage);
+document.addEventListener("DOMContentLoaded", setServiceName);
 document
-  .querySelectorAll(".menu, nav a, nav img")
+  .querySelectorAll(".menu, nav a")
   .forEach((e) => e.addEventListener("click", toggleNav));
 document.querySelector(".toggle-mode").addEventListener("click", toggleDark);
 document
   .querySelector(".toggle-language")
   .addEventListener("click", toggleLanguage);
+const services = require("./services.json");
+
+function setServiceName() {
+  const spanServiceName = document.querySelectorAll(".service-name");
+  const serviceName = new URLSearchParams(window.location.search).get("type");
+  for (const service in services) {
+    if (service === serviceName) {
+      spanServiceName.forEach((span) => (span.innerHTML = services[service]));
+    }
+  }
+}
+
+// cómo importar del script principal sin errores en consola
+function toggleNav() {
+  document.querySelector("nav").classList.toggle("hide");
+  document.querySelector(".button-menu").classList.toggle("opened");
+}
+
+function toggleDark() {
+  document.querySelector("body").classList.toggle("dark-mode");
+  document.querySelector("body").classList.toggle("light-mode");
+}
+
+function handleMode() {
+  if (window.matchMedia) {
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+    darkModeMediaQuery.addListener((e) => {
+      toggleDark();
+    });
+    if (darkModeMediaQuery.matches) {
+      document.querySelector("body").classList.toggle("dark-mode");
+    } else {
+      document.querySelector("body").classList.toggle("light-mode");
+    }
+  }
+}
