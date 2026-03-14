76ec06c143d00ca72d3f07dd3cfb5868c7ef83f8
saulo <sauloco022@gmail.com>
2026-03-14 12:23:19 -0300

    wip

---
+++ src/components/vue/ServicesView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MagicBento from "@components/vue/bits/MagicBento/MagicBento.vue";
+
+const deviceSize = 
+</script>
+
+<template>
+  <MagicBento
+      :text-auto-hide="true"
+      :enable-stars="false"
+      :enable-spotlight="true"
+      :enable-border-glow="true"
+      :enable-tilt="false"
+      :enable-magnetism="false"
+      :click-effect="true"
+      :spotlight-radius="100"
+      :particle-count="12"
+      glow-color="247,247,247"
+  />
+</template>
